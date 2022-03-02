2b6d92e9efc5a7edf5a78f02876b3227d6efb8c2
marcuskohlberg <marcus@encore.dev>
2022-03-02 15:35:31 +0100

    new

--- nickname/nickname.go
+++ nickname/nickname.go
@@ -21,29 +21,3 @@ func Name(ctx context.Context) (*Response, error) {
 type Response struct {
 	Nickname string
 }
-
-
-
-// ==================================================================
-
-// Encore comes with a built-in development dashboard for
-// exploring your API, viewing documentation, debugging with
-// distributed tracing, and more. Visit your API URL in the browser:
-//
-//     http://localhost:4000
-//
-
-// ==================================================================
-
-// Next steps
-//
-// 1. Deploy your application to the cloud with a single command:
-//
-//     git push encore
-//
-// 2. To continue exploring Encore, check out one of these topics:
-//
-//    Building a Slack bot:  https://encore.dev/docs/tutorials/slack-bot
-//    Building a REST API:   https://encore.dev/docs/tutorials/rest-api
-//    Using SQL databases:   https://encore.dev/docs/develop/sql-database
-//    Authenticating users:  https://encore.dev/docs/develop/auth
