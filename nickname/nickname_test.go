4ef918341f39e78573c66827cbc87838e6d6b5c2
marcuskohlberg <marcus@encore.dev>
2022-03-02 15:37:37 +0100

    removetest

--- nickname/nickname_test.go
+++ nickname/nickname_test.go
@@ -6,12 +6,5 @@ import (
 )
 
 func TestName(t *testing.T) {
-	resp, err := World(context.Background(), "Jane Doe")
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := "Hello, Jane Doe!"
-	if got := resp.Message; got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	
 }
