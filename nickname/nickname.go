7d3138f0b7e3238f4627c0d153fa90e43bc65583
marcuskohlberg <marcus@encore.dev>
2022-12-09 13:41:05 +0100

    Update nickname.go

--- nickname/nickname.go
+++ nickname/nickname.go
@@ -12,8 +12,8 @@ import (
 //encore:api public path=/nickname
 func Name(ctx context.Context) (*Response, error) {
 	
-	names := [20]string{"Chief", "Buddy", "Friend", "Ace", "Starlord", "Captain", "Snake",
-	 "Senior", "Junior", "Bud", "Ice", "Mario", "Big","Vanilla","Biscuit","Peanut","Bingo","Sting","Goose","Ducky"}
+	names := [21]string{"Chief", "Buddy", "Friend", "Ace", "Starlord", "Captain", "Snake",
+	 "Senior", "Junior", "Bud", "Ice", "Mario", "Big","Vanilla","Biscuit","Peanut","Bingo","Sting","Goose","Ducky","Lucky"}
 	poo := rand.Intn(len(names)-0) + 0
 	nick := names[poo]
 	return &Response{Nickname: nick}, nil
